8aa8c1336107edec541a005694f9fddc3d80884f
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2020-11-16 12:33:17 +0100

    again

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
         <meta property="og:title" :content="'Emiliano Maccaferri &mdash; ' + $route.meta.title">
         <meta property="og:site_name" :content="'Emiliano Maccaferri &mdash; ' + $route.meta.title">
         <meta property="og:see_also" content="https://emilianomaccaferri.com">
-        <meta itemprop="name" :content="$route.meta.title">
+        <meta itemprop="name" :content="'Emiliano Maccaferri &mdash;' + $route.meta.title">
         <meta itemprop="description" :content="$route.meta.description">
         <meta name="twitter:url" content="https://emilianomaccaferri.com">
         <meta name="twitter:title" :content="'Emiliano Maccaferri &mdash; ' + $route.meta.title">
