089f77348b6198dc0a2671675b3e2ec1c41cf3e7
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2021-07-24 19:39:47 +0200

    prepare for v2

---
+++ v2/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home'
+});
+</script>
