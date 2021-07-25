6b8a1101cc6bc4b457dff8013361cd593796f7ae
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2021-07-25 16:49:18 +0200

    doing things im not sure of

--- v2/src/App.vue
+++ v2/src/App.vue
@@ -1,6 +1,29 @@
 <template>
-  <router-view/>
+  <div style="display: flex;">
+    <Navbar />
+    <router-view/>
+  </div>
 </template>
+<script lang="ts">
+import gsap from 'gsap';
+import Navbar from '@/components/Navbar.vue'
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  components: { Navbar },
+  setup(props, context){
+    
+    const state = ref('first');
+
+    return { state }
+
+  },
+  provide(){
+    return {
+      state: this.state
+    }
+  }
+})
+</script>
 
 <style lang="scss">
   @import '@/assets/scss/style.scss';
--- v2/src/views/Home.vue
+++ v2/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <section>
-
+  <section class="full" style="width: 0;" @resize-navbar="theThing">
+    
   </section>
 </template>
 
@@ -8,6 +8,23 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'Home'
+  name: 'Home',
+  setup(props, context){
+
+    const age = Math.trunc(((Date.now() - new Date("03-20-2000").setHours(23, 0)) / 1000) / 31536000);
+
+    return { age }
+
+  },
+  provide(){
+    return {
+      age: this.age
+    }
+  },
+  methods: {
+    theThing(){
+      alert("aiuuu")
+    }
+  }
 });
 </script>
