b56db35158142c7525ff73a03eeaa4382a223e54
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2024-09-13 10:23:53 +0200

    add: v4 :)

---
+++ v4/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtPage />
+  </div>
+</template>
+<style lang="scss">
+  @import '@/assets/scss/style.scss';
+</style>
---
+++ v4/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <Title>emilianomaccaferri &mdash; chilling as a researcher</Title>
+    <h1 style="font-family: archivo; font-weight: bold;">Emiliano Maccaferri</h1>
+    <p style="font-family: archivo;">Archivo normal?</p>
+</template>
